b528a509e108d036f528a8b13730ab562fab9201
Daniel Zamanillo <dzamanillok@gmail.com>
2021-06-25 12:17:55 -0500

    add ability to add task

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Targeting button and saving the query to var. This saves comp resources
+var buttonEl = document.querySelector("#save-task");
+
+// Targeting ul and saving query to var
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li"); // Create var for a new list item
+  listItemEl.className = "task-item"; // gives list item a class
+  listItemEl.textContent = "This is a new task"; // gives list item text
+  tasksToDoEl.appendChild(listItemEl); // targets ul and adds new li of listItemEl to it
+};
+
+// Add Task Button
+buttonEl.addEventListener("click", createTaskHandler);
