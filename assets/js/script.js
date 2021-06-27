6bb82faaad595ad642ba8ef6d63ef29411809888
Daniel Zamanillo <dzamanillok@gmail.com>
2021-06-27 09:55:43 -0500

    type added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,33 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Fuction for add task button
 var createTaskHandler = function (event) {
-  event.preventDefault();
+  event.preventDefault(); // prevents browser refresh on button click
 
-  var listItemEl = document.createElement("li"); // Create var for a new list item
-  listItemEl.className = "task-item"; // gives list item a class
-  listItemEl.textContent = "This is a new task"; // gives list item text
-  tasksToDoEl.appendChild(listItemEl); // targets ul and adds new li of listItemEl to it
+  var taskNameInput = document.querySelector("input[name='task-name']").value; // targets value of user input on form
+
+  var taskTypeInput = document.querySelector("select[name='task-type']").value; // targets value of task type from drop down
+  console.log(taskTypeInput);
+
+  // create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  // give it a class name
+  taskInfoEl.className = "task-info";
+  // add HTML content to div
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 // Add Task Button
